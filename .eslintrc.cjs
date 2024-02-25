3a9c191faffee1af7ab7d84ae2492fca4d7389eb
Ivan Priyambudi <gendoh75@gmail.com>
2024-02-25 14:43:38 +0700

    feat: up

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxt/eslint-config"],
+  plugins: [],
+  rules: {},
+};
