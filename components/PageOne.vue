1e6372d434f1e4e9bcf0646dc7ebb82541c9c378
Ivan Priyambudi <gendoh75@gmail.com>
2024-02-25 16:01:02 +0700

    feat: up

--- components/PageOne.vue
+++ components/PageOne.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    Pag 1
+    <div style="display: flex; flex-direction: column; align-items: center;">
+      <text-ivan :keys="'0'" />
+      <text-and2 :keys="'0'" />
+      <text-karina :keys="'0'" />
+
+      <div
+        class="contents"
+        style="text-align: center; color: #FFF; margin-top: 2vh;"
+      >
+        <p>
+          Di antara tanda-tanda (kebesaran)-Nya ialah bahwa Dia menciptakan
+          pasangan-pasangan untukmu dari (jenis) dirimu sendiri agar kamu
+          merasa tenteram kepadanya. Dia menjadikan di antaramu rasa cinta dan
+          kasih sayang. Sesungguhnya pada yang demikian itu benar-benar
+          terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.
+        </p>
+        <h6>(QS Ar-Rum · Ayat 21)</h6>
+      </div>
+    </div>
   </div>
 </template>
